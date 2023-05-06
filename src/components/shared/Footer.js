c4ca1a5e768f43bbb044c46fcbbd390c71201a12
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2023-05-06 01:10:16 -0400

    footerrrrr :')

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,9 +1,14 @@
-import { Heading, Flex, Image, Box, Spacer, Divider, Text, HStack, VStack } from '@chakra-ui/react';
+import {
+  Heading, Flex, Image, Box, Spacer, Divider, Text, HStack, VStack,
+  Grid, GridItem, useBreakpointValue,
+} from '@chakra-ui/react';
 import NextImage from 'next/image';
 import Logo from '../../../public/logo-group.png';
 import NavBar from './FooterNavBar';
 
 export default function Footer() {
+  const isDesktop = useBreakpointValue({ md: true, base: false });
+
   return (
     <Flex
       direction="column"
@@ -23,12 +28,29 @@ export default function Footer() {
       <VStack spacing={6} w="100%">
         <NavBar />
         <Divider />
-        <Flex w="100%" justifyContent="space-between">
-          <Text color="#C2D5ED">&copy; 2023 Kaizen Capital Partners. All Rights Reserved.</Text>
-          <HStack spacing={8}>
-            <Text color="#C2D5ED">Terms & Conditions</Text>
-            <Text color="#C2D5ED">Privacy Policy</Text>
-          </HStack>
+        <Flex w="100%" justifyContent="space-between" direction={{ base: 'column', md: 'row' }}>
+          {isDesktop && (
+            <>
+              <Text color="#C2D5ED">&copy; 2023 Kaizen Capital Partners. All Rights Reserved.</Text>
+              <HStack spacing={8} alignSelf="center">
+                <Text color="#C2D5ED">Terms & Conditions</Text>
+                <Text color="#C2D5ED">Privacy Policy</Text>
+              </HStack>
+            </>
+          )}
+          {!isDesktop && (
+            <>
+              <Grid w="100%" templateColumns="repeat(2, 1fr)" gap={0} pb={4}>
+                <GridItem w="100%" borderRight="1px solid #C2D5ED" display="flex" pr={4}>
+                  <Text w="100%" color="#C2D5ED" align="end">Terms & Conditions</Text>
+                </GridItem>
+                <GridItem borderLeft="1px solid #C2D5ED" w="100%" pl={4}>
+                  <Text w="100%" color="#C2D5ED" align="start">Privacy Policy</Text>
+                </GridItem>
+              </Grid>
+              <Text alignSelf="center" color="#C2D5ED">&copy; 2023 Kaizen Capital Partners. All Rights Reserved.</Text>
+            </>
+          )}
         </Flex>
       </VStack>
     </Flex>
