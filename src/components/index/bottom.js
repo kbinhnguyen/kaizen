dab00a6f466a9abe714c08597e30dbafe4f53e16
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2023-05-05 23:32:07 -0400

    bottom panel index

--- src/components/index/bottom.js
+++ src/components/index/bottom.js
@@ -4,49 +4,59 @@ import bg from '../../../public/AdobeStock_386588109.jpeg';
 
 const style = {
   height: '100%',
-  width: '100%',
+  // width: '100%',
   backgroundImage: `url(${bg.src})`,
   backgroundPosition: 'center',
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
-  // position: 'relative',
 };
 
 export default function BottomPanel() {
   return (
   <>
-    <Grid w="100vw" h="100vh" minHeight="800px" templateRows="5fr 2fr" gap={14}>
+    <Grid
+      w="100vw"
+      h={{ base: 'max-content', md: '100vh' }}
+      minHeight={{ base: 'max-content', md: '800px'}}
+      templateRows={{ base: '500px 550px', md: '5fr 2fr'}}
+      gap={{ base: 0, md: 14 }}
+    >
       <GridItem w="100%" h="100%" style={style} display="flex" justifyContent="end">
-        <VStack px={20} pt={32} w="45%" spacing={8}>
+        <VStack pr={{ base: 10, md: 20}} pt={{ base: 20, md: 32 }} w={{ base: '65%', md: '45%' }} spacing={8}>
           <Heading variant="tagline" as="h2">Investing in and building business relationships with people who share our values.</Heading>
           <Box w="100%">
             <Link variant="learnMore" w="max-content" as={NextLink} href="/contact">Schedule a Meeting</Link>
           </Box>
         </VStack>
       </GridItem>
-      <GridItem w="100%" h="100%" px={20}>
-        <Grid templateColumns="7fr repeat(3, 5fr)" gap={14}>
+      <GridItem w="100%" h="100%" px={{ base: 10, md: 20 }} display="flex" alignItems="center">
+        <Grid
+          templateColumns={{ base: '1fr', md: '7fr repeat(3, 5fr)' }}
+          templateRows={{ base: 'repeat(4, max-content)', md: '1fr' }}
+          gap={{ base: 8, md: 14 }}
+          pb={{ base: 8, md: 0 }}
+        >
           <GridItem>
-            <VStack spacing={6} w="65%">
+            <VStack spacing={{ base: 2, md: 6 }} w={{ base: '80%', md: '65%' }}>
               <Heading as="h2" w="100%" color="#33647E">Business Strategy</Heading>
               <Divider borderColor="#33647E" />
             </VStack>
           </GridItem>
           <GridItem>
             <VStack spacing={3}>
-              <Heading as="h4" fontSize="28px" w="100%" color="#33647E">Investments</Heading>
+              <Heading as="h4" fontSize={{ base: '24px', md: '28px' }} w="100%" color="#33647E">Investments</Heading>
               <Text fontSize="18px">We seek opportunities to invest in and build business relationships with people who share our values.</Text>
             </VStack>
           </GridItem>
           <GridItem>
             <VStack spacing={3}>
-              <Heading as="h4" fontSize="28px" w="100%" color="#33647E">Consulting</Heading>
+              <Heading as="h4" fontSize={{ base: '24px', md: '28px' }} w="100%" color="#33647E">Consulting</Heading>
               <Text fontSize="18px">We always look forward to sharing our knowledge and expertise.</Text>
             </VStack>
           </GridItem>
           <GridItem>
             <VStack spacing={3}>
-              <Heading as="h4" fontSize="28px" w="100%" color="#33647E">Investors</Heading>
+              <Heading as="h4" fontSize={{ base: '24px', md: '28px' }} w="100%" color="#33647E">Investors</Heading>
               <Text fontSize="18px">We seek investors who want to help others grow and prosper.</Text>
             </VStack>
           </GridItem>
