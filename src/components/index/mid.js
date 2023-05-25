428b44b25eff77847d8792fbb40303326227415d
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2023-05-24 23:39:11 -0400

    grid layout for ipads

--- src/components/index/mid.js
+++ src/components/index/mid.js
@@ -48,22 +48,25 @@ export default function MidPanel() {
   return (
     <Grid
       w="100vw"
-      minHeight={{ base: 'max-content', md: '1600px' }}
-      h={{ md: '200vh', base: 'max-content'}}
-      templateRows={{ base: 'repeat(2, max-content)', md: 'repeat(2, 1fr)'}}
+      minHeight={{ base: 'max-content', lg: '1600px' }}
+      h={{ lg: '200vh', base: 'max-content'}}
+      templateRows={{ base: 'repeat(2, max-content)', lg: 'repeat(2, 1fr)'}}
       templateColumns="1fr"
+      gap={{ lg: 0, base: 6 }}
+      py={{ base: 6, lg: 0 }}
     >
       <GridItem w="100%" h="100%">
         <Grid
-          templateRows={{base: 'repeat(2, max-content)', md: '1fr' }}
-          templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)'}}
+          templateRows={{base: 'repeat(2, max-content)', lg: '1fr' }}
+          templateColumns={{ base: '1fr', lg: 'repeat(2, 1fr)'}}
           w="100%" h="100%"
           className="eleToWatch"
+          gap={{ lg: 0, base: 6 }}
         >
-          <GridItem w="100%" pl={{ base: 10, md: 20}} pr={{ base: 10, lg: 20 }}>
+          <GridItem w="100%" pl={{ base: 10, lg: 20}} pr={{ base: 10, lg: 20 }}>
             <Stack
-              spacing={{ base: 2, md: 6 }}
-              pt={{ base: 12, md: 44 }}
+              spacing={{ base: 2, lg: 6 }}
+              pt={{ base: 12, lg: 44 }}
               direction="column"
             >
               <Heading variant="accented" as="h3" w="100%" fontSize={{ base: '16px', sm: '18px', md: '20px' }}>OUR APPROACH</Heading>
@@ -74,15 +77,15 @@ export default function MidPanel() {
             </Stack>
           </GridItem>
           <GridItem
-            h={{base: '400px', md: '100%' }}
-            mr={{ base: 20, md: 20 }}
-            ml={{ base: 20, md: 0 }}
-            my={{ base: 4, md: 0 }}
-            pt={{ base: 0, md: 6 }}
+            h={{base: '400px', lg: '100%' }}
+            mr={20}
+            ml={{ base: 20, lg: 0 }}
+            my={{ base: 4, lg: 0 }}
+            pt={{ base: 0, lg: 6 }}
             display="flex"
             alignItems="center"
           >
-            <Box h={{base: '100%', md: '95%'}} w="100%" position="relative">
+            <Box h={{base: '100%', lg: '95%'}} w="100%" position="relative">
               <NextImage
                 style={{objectFit: 'cover'}}
                 fill={true}
@@ -95,21 +98,22 @@ export default function MidPanel() {
       </GridItem>
       <GridItem w="100%" h="100%">
         <Grid
-          templateRows={{base: 'repeat(2, max-content)', md: '1fr' }}
-          templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)'}}
+          templateRows={{base: 'repeat(2, max-content)', lg: '1fr' }}
+          templateColumns={{ base: '1fr', lg: 'repeat(2, 1fr)'}}
           w="100%" h="100%"
+          gap={{ lg: 0, base: 6 }}
         >
           <GridItem
-            h={{base: '400px', md: '100%' }}
-            ml={{ base: 20, md: 20 }}
-            mr={{ base: 20, md: 0 }}
-            my={{ base: 4, md: 0 }}
-            pb={{ base: 0, md: 6 }}
+            h={{base: '400px', lg: '100%' }}
+            ml={{ base: 20, lg: 20 }}
+            mr={{ base: 20, lg: 0 }}
+            my={{ base: 4, lg: 0 }}
+            pb={{ base: 0, lg: 6 }}
             display="flex"
             alignItems="center"
             className="eleToWatch"
           >
-            <Box h={{base: '100%', md: '95%'}} w="100%" position="relative">
+            <Box h={{base: '100%', lg: '95%'}} w="100%" position="relative">
               <NextImage
                 style={{objectFit: 'cover'}}
                 fill={true}
@@ -118,16 +122,16 @@ export default function MidPanel() {
               />
             </Box>
           </GridItem>
-          <GridItem pr={{ base: 10, md: 20}} pl={{ base: 10, md: 12 }} pb={{ base: 12, md: 0 }}>
+          <GridItem pr={{ base: 10, lg: 20}} pl={{ base: 10, lg: 12 }} pb={{ base: 12, lg: 0 }}>
             <Stack
-              pt={{ base: 4, md: 36, lg: 48 }}
-              spacing={{ base: 4, md: 12 }}
-              h={{ base: 'max-content', md: '60%'}}
-              direction={{ base: 'column', md: 'row' }}
+              pt={{ base: 4, lg: 36, lg: 48 }}
+              spacing={{ base: 4, lg: 12 }}
+              h={{ base: 'max-content', lg: '60%'}}
+              direction={{ base: 'column', lg: 'row' }}
               display="flex"
-              alignItems={{ base: 'center', md: 'start' }}
+              alignItems={{ base: 'center', lg: 'start' }}
             >
-              <Stack w="max-content" spacing={1} direction={{ base:'row', md: 'column' }} pt={{ md: 32, lg: 20 }}>
+              <Stack w="max-content" spacing={1} direction={{ base:'row', lg: 'column' }} pt={{ lg: 20 }}>
                 <CircleIcon
                   color={section == 0 ? '#33647E' : '#DCE3EB'}
                   onClick={() => setSection(0)}
